283eb2f714c10f870c76b46d71e954c03acfe90e
Alon Titelman <alon.titelman@starkware.co>
2024-06-19 14:57:08 +0300

    Added output of public / private input to app.

--- src/bin/run_simple_bootloader/main.rs
+++ src/bin/run_simple_bootloader/main.rs
@@ -1,8 +1,10 @@
 use std::env;
 use std::error::Error;
+use std::io::{self, Write};
 use std::path::PathBuf;
 
-use cairo_vm::cairo_run::{cairo_run_program_with_initial_scope, CairoRunConfig};
+use bincode::enc::write::Writer;
+use cairo_vm::cairo_run::{self, cairo_run_program_with_initial_scope, CairoRunConfig};
 use cairo_vm::types::exec_scope::ExecutionScopes;
 use cairo_vm::types::layout_name::LayoutName;
 use cairo_vm::types::program::Program;
@@ -15,6 +17,7 @@ use cairo_bootloader::{
     insert_simple_bootloader_input, BootloaderHintProcessor, SimpleBootloaderInput, TaskSpec,
 };
 use clap::Parser;
+use tempfile::NamedTempFile;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -30,18 +33,51 @@ struct Args {
     #[clap(long = "layout", default_value = "plain", value_enum)]
     layout: LayoutName,
 }
+struct FileWriter {
+    buf_writer: io::BufWriter<std::fs::File>,
+    bytes_written: usize,
+}
+
+impl Writer for FileWriter {
+    fn write(&mut self, bytes: &[u8]) -> Result<(), bincode::error::EncodeError> {
+        self.buf_writer
+            .write_all(bytes)
+            .map_err(|e| bincode::error::EncodeError::Io {
+                inner: e,
+                index: self.bytes_written,
+            })?;
+
+        self.bytes_written += bytes.len();
+
+        Ok(())
+    }
+}
+
+impl FileWriter {
+    fn new(buf_writer: io::BufWriter<std::fs::File>) -> Self {
+        Self {
+            buf_writer,
+            bytes_written: 0,
+        }
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        self.buf_writer.flush()
+    }
+}
 
 fn cairo_run_simple_bootloader_in_proof_mode(
     simple_bootloader_program: &Program,
     tasks: Vec<TaskSpec>,
+    layout: LayoutName,
 ) -> Result<CairoRunner, CairoRunError> {
     let mut hint_processor = BootloaderHintProcessor::new();
 
     let cairo_run_config = CairoRunConfig {
         entrypoint: "main",
-        trace_enabled: false,
-        relocate_mem: false,
-        layout: LayoutName::all_cairo,
+        trace_enabled: true,
+        relocate_mem: true,
+        layout,
         proof_mode: true,
         secure_run: None,
         disable_trace_padding: false,
@@ -91,7 +127,42 @@ fn main() -> Result<(), Box<dyn Error>> {
         args.use_poseidon,
     )?;
 
-    let mut _runner = cairo_run_simple_bootloader_in_proof_mode(&simple_bootloader_program, tasks)?;
+    let runner =
+        cairo_run_simple_bootloader_in_proof_mode(&simple_bootloader_program, tasks, args.layout)?;
+
+    std::fs::write(
+        args.air_public_input,
+        runner.get_air_public_input()?.serialize_json()?,
+    )?;
+
+    let relocated_trace = runner
+        .relocated_trace
+        .as_ref()
+        .expect("trace not relocated");
+
+    let (trace_file, trace_file_path) = NamedTempFile::new()?.keep()?;
+
+    let mut trace_writer =
+        FileWriter::new(io::BufWriter::with_capacity(3 * 1024 * 1024, trace_file));
+
+    cairo_run::write_encoded_trace(relocated_trace, &mut trace_writer)?;
+    trace_writer.flush()?;
+
+    let (memory_file, memory_file_path) = NamedTempFile::new()?.keep()?;
+    let mut memory_writer =
+        FileWriter::new(io::BufWriter::with_capacity(5 * 1024 * 1024, memory_file));
+
+    cairo_run::write_encoded_memory(&runner.relocated_memory, &mut memory_writer)?;
+    memory_writer.flush()?;
+
+    let json = runner
+        .get_air_private_input()
+        .to_serializable(
+            trace_file_path.to_string_lossy().to_string(),
+            memory_file_path.to_string_lossy().to_string(),
+        )
+        .serialize_json()?;
+    std::fs::write(args.air_private_input, json)?;
 
     Ok(())
 }
